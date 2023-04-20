eac219cc845589c0765cb5417dc5ca40c6381c06
Pedro Simbera <124898848+PedroSimbera@users.noreply.github.com>
2023-04-20 17:36:24 -0300

    Add files via upload

---
+++ calc_retangulo/src/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class main {
+public static void main(String[] args) {
+
+
+	Scanner sc = new Scanner(System.in);
+		
+	double base, altura, area, diagonal, perimetro;
+		
+	System.out.print("Digite a base do retangulo: ");
+	base = sc.nextDouble();
+	System.out.print("Digite a altura do retangulo: ");
+	altura = sc.nextDouble();
+	            
+	area = base * altura;
+	perimetro = 2* (base + altura);
+	diagonal = Math.sqrt(Math.pow(base, 2.0) + Math.pow(altura, 2.0));
+			
+	System.out.println("A área: " + area + ". O perímetro: " + perimetro + ". A diagonal: " + diagonal);
+		
+	sc.close();
+}
+}
